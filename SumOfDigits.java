4b488da74c658f98a637722a307964de8ae0a777
Julianne <43345113+JulianneW@users.noreply.github.com>
2020-04-15 12:45:34 -0400

    Update SumOfDigits.java

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -3,22 +3,32 @@
  * SumOfDigits.java
  * 14/04/2020
  */
- 
+
 import java.util.Scanner;
 public class Digit_Sum 
 {
-    public static void main(String args[])
+    int sum = 0;
+    public static void main(String[] args) 
     {
-        int m, n, sum = 0;
+        int n;
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the number:");
-        m = s.nextInt();
-        while(m > 0)
+        n = s.nextInt();
+        Digit_Sum obj = new Digit_Sum();
+        int a = obj.add(n);
+        System.out.println("Sum:"+a);
+    }
+    int add(int n)
+    {
+        sum = n % 10;
+        if(n == 0)
         {
-            n = m % 10;
-            sum = sum + n;
-            m = m / 10;
+            return 0;
         }
-        System.out.println("Sum of Digits:"+sum);
+        else
+        {
+             return sum + add(n / 10);
+        }
+ 
     }
 }
